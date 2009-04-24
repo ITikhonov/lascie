8e8381edc44d67ac44f295fd81474dc069abccf8
kef@localhost.localdomain <>
2009-04-24 14:03:26 +0600

    branch:      data data viewing with cosmetic bugs and weird code

--- lasca.c
+++ lasca.c
@@ -10,10 +10,10 @@
 static cairo_t *cr=0;
 int button_height=0;
 
-struct nm { char s[8]; int def[10]; } nms[100];
+struct nm { char s[8]; int def[10]; int data; } nms[100];
 struct nm *nme=nms;
 
-struct bt { int x,y; struct nm *n; void (*act)(void); int x2,y2,open;} bts[100];
+struct bt { int x,y; struct nm *n; void (*act)(void); int x2,y2,open,pos;} bts[100];
 struct bt *bte=bts;
 
 struct ops { void (*button)(int,int); void (*key)(int); };
@@ -58,6 +58,9 @@ void do_edit() {
 	draw();
 }
 
+void do_down() { if(which) { if((which->pos+8)<which->n->def[1]) { which->pos+=8; draw(); } } }
+void do_up() { if(which) { if((which->pos-8)>=0) { which->pos-=8; draw(); } } }
+
 void button_edit(int x, int y) {
 	hit(x,y); if(clicked) {
 		if(which->n->def[editpos]==-1) { which->n->def[editpos+1]=-1; }
@@ -86,7 +89,10 @@ void add(int x, int y, char *s, void (*f)(void)) {
 	struct bt *w=bte; w->n=nme; bte++; nme++;
 	w->x=x; w->y=y; strncpy(w->n->s,s,8); w->act=f;
 	w->n->def[0]=-1;
+	w->n->data=0;
 }
+
+unsigned char hexext[256];
 
 void init(cairo_t *cr1) {
 	add(30,30,"create", do_create);
@@ -95,12 +101,21 @@ void init(cairo_t *cr1) {
 	add(30,90,"rename", do_rename);
 	add(30,110,"open", do_open);
 	add(30,130,"edit", do_edit);
+	add(30,150,"up", do_up);
+	add(30,170,"down", do_down);
 
 	add(90,90,"test", do_choose);
 	bte[-1].n->def[0]=0;
 	bte[-1].n->def[1]=1;
 	bte[-1].n->def[2]=2;
 	bte[-1].n->def[3]=-1;
+
+	add(150,90,"dtest", do_choose);
+	char *p=(char *)((bte[-1].n->def[0])=(int)malloc(17));
+	{ int i; for(i=0;i<17;i++) p[i]=i; }
+	bte[-1].n->def[1]=17;
+	bte[-1].n->data=1;
+	bte[-1].pos=8;
 
 	cr=cr1;
 	cairo_select_font_face (cr, "times", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
@@ -110,23 +125,43 @@ void init(cairo_t *cr1) {
 	cairo_text_extents_t te;
 	cairo_text_extents(cr,"abcdefghijklmnopqrstuvwxyz0123456789;",&te);
 	button_height=te.height;
+
+	char s[4];
+	int n;
+	for(n=0;n<0x100;n++) { sprintf(s," %02hhx",n); cairo_text_extents(cr,s,&te); hexext[n]=te.x_advance; }
 }
 
 void draw_button(struct bt *bt) {
 	char *s=bt->n->s;
 	int ws[11], wn=1;
+	char ns[10];
 
 	cairo_text_extents_t te;
 	cairo_text_extents(cr,s,&te);
 	ws[0]=te.width;
 
 	if(bt->open) {
-		int *n=bt->n->def;
-		int *p=ws+1;
-		while(*n != -1) {
-			cairo_text_extents(cr,nms[*n++].s,&te);
-			*p++=te.width;
+		if(!bt->n->data) {
+			int *n=bt->n->def;
+			int *p=ws+1;
+			while(*n != -1) {
+				cairo_text_extents(cr,nms[*n++].s,&te);
+				*p++=te.width;
+				wn++;
+			}
+		} else {
+			sprintf(ns,"%x:",bt->n->def[1]);
+			cairo_text_extents(cr,ns,&te);
+			ws[1]=te.x_advance;
 			wn++;
+
+			char *p=(char *)(bt->n->def[0])+bt->pos;
+			int m=bt->n->def[1];
+			if(m>8) m=8;
+			char *e=p+m;
+			ws[2]=0;
+			wn++;
+			while(p<e) { ws[2]+=hexext[(unsigned char)(*p++)]; }
 		}
 	}
 
@@ -148,20 +183,37 @@ void draw_button(struct bt *bt) {
 
 	if(bt->open) {
 		x+=ws[0];
-		int *n=bt->n->def;
-		int *p=ws+1;
-		while(*n != -1) {
-			x+=5;
-			if(ops==&edit&&which==bt&&(p-ws-1)==editpos) {
-				cairo_set_source_rgb(cr,0.9,0.9,0.5);
-				cairo_rectangle(cr,x,bt->y+2,*p,button_height+1);
-				cairo_fill(cr);
+		if(!bt->n->data) {
+			int *n=bt->n->def;
+			int *p=ws+1;
+			while(*n != -1) {
+				x+=5;
+				if(ops==&edit&&which==bt&&(p-ws-1)==editpos) {
+					cairo_set_source_rgb(cr,0.9,0.9,0.5);
+					cairo_rectangle(cr,x,bt->y+2,*p,button_height+1);
+					cairo_fill(cr);
+				}
+				cairo_set_source_rgb(cr,0,0,0);
+				cairo_move_to (cr, x, y);
+				cairo_show_text(cr,nms[*n++].s);
+				cairo_stroke(cr);
+				x+=(*p++);
 			}
-			cairo_set_source_rgb(cr,0,0,0);
-			cairo_move_to (cr, x, y);
-			cairo_show_text(cr,nms[*n++].s);
-			cairo_stroke(cr);
-			x+=(*p++);
+		} else {
+				x+=5;
+				cairo_move_to (cr, x, y);
+				cairo_show_text(cr,ns);
+
+				cairo_set_source_rgb(cr,0,0,0.5);
+				char *p=(char *)(bt->n->def[0]);
+				char *e=p+bt->n->def[1];
+				p+=bt->pos;
+				if((p+8)>=e) { p=e-8-1; }
+				else { e=p+8; }
+				
+				char s[4]; while(p<e) { sprintf(s," %02hhx",*p++); cairo_show_text(cr,s); }
+
+				cairo_stroke(cr);
 		}
 	}
 }
